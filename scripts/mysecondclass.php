6ad2df8620d2bbc175782b014d13ef84d91f061e
Roman Malkov <roman-li-83@yandex.ru>
2021-02-03 00:19:51 +0500

    sync local version with git and with phpStorm

--- scripts/mysecondclass.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-class MySecondClass extends MyFirstClass {
-	public function newMetod(){
-		echo"Из нового метода класса " . __CLASS__ . ".<br />";
-	}
-} 
-
-?>
--- scripts/test_ajax.php
+++
@@ -1,6 +0,0 @@
-<?php 
- $a=$_GET['x'];
- $b=$_GET['y'];
- $result = $a+$b;
-echo($result);
-?> 
